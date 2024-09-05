c5bd51b5110bfb24c83dbe62c0ee980aad7de4c0
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-09-05 10:05:37 +0700

    Add Header to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,15 @@
 import type { Metadata } from 'next'
 import localFont from 'next/font/local'
+
 import './globals.css'
+import Header from '../components/Header'
 
 const geistSans = localFont({
 	src: './fonts/GeistVF.woff',
 	variable: '--font-geist-sans',
 	weight: '100 900',
 })
+
 const geistMono = localFont({
 	src: './fonts/GeistMonoVF.woff',
 	variable: '--font-geist-mono',
@@ -28,6 +31,7 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
+				<Header />
 				{children}
 			</body>
 		</html>
